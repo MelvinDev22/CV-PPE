e33e0ce2a71d8537d23f0ecf0ed281e3fa7761be
Melvin <dev.mhenaff@gmail.com>
2019-11-08 20:08:31 +0100

    Page Html + CSS + JS

---
+++ PageCV3.js
@@ -0,0 +1,7 @@
+function mouseOver() {
+	var str = document.getElementById("birthdate").textContent = "20 ans";
+}
+
+function mouseOut() {
+	var str = document.getElementById("birthdate").textContent = "Né le 07/11/1998";
+}
